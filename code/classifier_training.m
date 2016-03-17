fe7ed0693847eb72f32e04ecaee95229ba9112b3
Mingfei SUN <mingfei.sun.hk@gmail.com>
2016-03-17 22:12:15 +0800

    replace codes in proj4 as a function classifier_training

---
+++ code/classifier_training.m
@@ -0,0 +1,6 @@
+%% the classifier_training function
+
+function [w, b] = classifier_training(features_pos, features_neg)
+
+w = rand((feature_params.template_size / feature_params.hog_cell_size)^2 * 31,1); %placeholder, delete
+b = rand(1); %placeholder, delete
--- code/proj4.m
+++ code/proj4.m
@@ -85,8 +85,7 @@ features_neg = get_random_negative_features( non_face_scn_path, feature_params, 
 
 %YOU CODE classifier training. Make sure the outputs are 'w' and 'b'.
 % TODO
-w = rand((feature_params.template_size / feature_params.hog_cell_size)^2 * 31,1); %placeholder, delete
-b = rand(1); %placeholder, delete
+[w, b] = classifier_training(features_pos, features_neg)
 
 %% step 3. Examine learned classifier
 % You don't need to modify anything in this section. The section first
